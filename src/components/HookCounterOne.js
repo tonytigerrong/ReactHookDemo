4805609a19271679d650c5da31dcc208a3b90637
jianzhong.rong <tony.tiger.rong@gmail.com>
2020-05-15 23:09:58 -0300

    8 conditional run useEffect useEffect( arrowFunciton, [state element1, state element2] ) useEffect will be called ONLY when element1 or element2 updated

--- src/components/HookCounterOne.js
+++ src/components/HookCounterOne.js
@@ -2,12 +2,19 @@ import React,{ useState, useEffect } from 'react'
 
 function HookCounterOne() {
     const [count, setCount] = useState(0);
+    const [name, setName] = useState('')
     // componentDidMount DidUpdate 
     useEffect(()=>{
+        console.log("useEffect - update title")
         document.title = `You clicked ${count} time`;
-    });
+    },[count]);
     return (
         <div>
+            <input type="text" onChange={
+                (e)=>{
+                    setName(e.target.value)
+                }
+            }/>
             <button onClick={
                 ()=> setCount(count+1)
             }>click {count} times</button>
