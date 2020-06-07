f3c449f98b951760af243c6d8a1a8166ca10ff72
jianzhong.rong <tony.tiger.rong@gmail.com>
2020-06-07 17:33:45 -0300

    call a function in useEffect, definit the function in useEffect and add parameter of the function into dependency array

--- src/components/IntervalHookCounter.js
+++ src/components/IntervalHookCounter.js
@@ -2,6 +2,7 @@ import React,{useState, useEffect} from 'react'
 
 function IntervalHookCounter() {
     const [count, setCount] = useState(0)
+    const [otherProps, setOtherProps] = useState(null)
     const tick = ()=>{
         // setCount(count+1);
         // or we can use precount to avoid add dependency array to useEffect
@@ -9,13 +10,21 @@ function IntervalHookCounter() {
     }
 
     useEffect(()=>{
+        //if we call a function in useEffect, we should include the function definition into
+        //useEffect, so we won't forget add the props value into dependency array
+        function doSomething(){
+            console.log(otherProps);
+        }
         console.log('start effect')
         const interval = setInterval(tick,1000)
         return ()=>{
             console.log('end effect')
             clearInterval(interval)
         }
-    },[]) // the 2nd parameter is dependency array, indicate when we run useEffect
+    },[
+        // count,
+        otherProps
+    ]) // the 2nd parameter is dependency array, indicate when we run useEffect
 
 
     return (
