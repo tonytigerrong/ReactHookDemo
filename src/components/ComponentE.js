704d3782e60b59a74c779e8d7110470675756d49
jianzhong.rong <tony.tiger.rong@gmail.com>
2020-06-10 13:18:14 -0300

    22 useReducer & useContext      App         - A         - B         - C              - E (invoke dispatch define in A )

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import ComponentF from './ComponentF'
+import { CountContext } from '../App'
 
 export default function ComponentE() {
+    const countContext = useContext(CountContext);
+    
     return (
         <div>
             <ComponentF />
+            <button onClick={()=>{
+                countContext.countDispatch('inc')
+            }}>inc(inComponentE)</button>
         </div>
     )
 }
