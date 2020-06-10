21df484c3b830c5a62f275dd0440c7f7787c7d19
jianzhong.rong <tony.tiger.rong@gmail.com>
2020-06-10 19:32:47 -0300

    26 useCallback hook      wrip function with useCallback and add dependency parameter      then the function will be chage(reload) only when dependency changed

--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -1,15 +1,15 @@
-import React,{useState } from 'react'
+import React,{useState,useCallback } from 'react'
 import Counter from './Counter'
 import Button from './Button'
 export default function ParentComponent() {
     const [age, setAge] = useState(21)
     const [salary, setSalary] = useState(50000)
-    const incrementAge = ()=>{
+    const incrementAge = useCallback(()=>{
         setAge(age+1)
-    }
-    const incrementSalary = ()=>{
+    },[age]) // age is dependency, the function rerendered,only when the dependency changed 
+    const incrementSalary = useCallback( ()=>{
         setSalary(salary+1000)
-    }
+    },[salary])
     return (
         <div>
             <Counter text='age' count={age}/>
